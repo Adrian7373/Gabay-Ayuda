2263dc7fc128731009a1bc670ce9c59aa5f2628a
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-16 22:10:26 +0800

    Implement data fetching in SuperDashboard; retrieve profiles and sessions from Supabase

--- src/app/dashboard/components/SuperDashboard/page.tsx
+++ src/app/dashboard/components/SuperDashboard/page.tsx
@@ -1,6 +1,26 @@
 import style from "./page.module.css";
+import { createClient } from "@/utils/supabase/server";
 
-export default function SuperDashboard() {
+export default async function SuperDashboard() {
+
+    const supabase = await createClient();
+
+    const { data: profiles, error: profileFetchError } = await supabase
+        .from("profiles")
+        .select("name, role");
+
+    if (!profiles || profileFetchError) {
+        throw new Error("Error fetching profiles");
+    }
+
+    const { data: sessions, error: sessionFetchError } = await supabase
+        .from("batches")
+        .select("created_at, name, max_approved, verification_code, deadline, is_active");
+
+    if (!sessions || sessionFetchError) {
+        throw new Error("Error fetching sessions");
+    }
+
     return (
         <div className={style.mainDiv}>
             Super Dashboard
