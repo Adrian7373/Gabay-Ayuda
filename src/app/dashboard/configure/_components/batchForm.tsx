26d04dfe8adc9c23891b5fdfc20cbfb6dde955a1
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-19 21:09:33 +0800

    Add BatchForm component for creating new batches and integrate it into the Configure page

---
+++ src/app/dashboard/configure/_components/batchForm.tsx
@@ -0,0 +1,42 @@
+"use client";
+import style from "./batchForm.module.css";
+import { createBatch } from "@/app/actions";
+
+export default function BatchForm({ }) {
+    return (
+        <div className={style.mainDiv}>
+            <div className={style.header}>
+                <p>CREATE A NEW BATCH/SESSION</p>
+                <p>{profile.name}</p>
+            </div>
+            <div className={style.formDiv}>
+                <form action={createBatch}>
+                    <label>Name:
+                        <input name="name" type="text" required />
+                    </label>
+                    <label>Max Beneficiaries:
+                        <input name="max_ben" type="text" />
+                    </label>
+                    <label>Set Verification Code:
+                        <input name="code" type="text" required />
+                    </label>
+                    <label>Deadline:
+                        <input name="deadline" type="date" />
+                    </label>
+                    <label>Assign Admin:
+                        <select name="assignedAdmin">
+                            {!profiles ? (
+                                <option value="" disabled >No admin available</option>
+                            ) : (
+                                profiles.map((profile) => (
+                                    <option key={profile.id} value={profile.id}>{profile.name}</option>
+                                ))
+                            )}
+                        </select>
+                    </label>
+                    <button type="submit">Create</button>
+                </form>
+            </div>
+        </div>
+    )
+}
--- src/app/dashboard/configure/page.tsx
+++ src/app/dashboard/configure/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import style from "./page.module.css";
 import { createClient } from "@/utils/supabase/server";
 import { createBatch } from "@/app/actions";
+import BatchForm from "./_components/batchForm";
 
 export default async function Configure() {
 
@@ -29,38 +30,7 @@ export default async function Configure() {
 
     return (
         <div className={style.mainDiv}>
-            <div className={style.header}>
-                <p>CREATE A NEW BATCH/SESSION</p>
-                <p>{profile.name}</p>
-            </div>
-            <div className={style.formDiv}>
-                <form action={createBatch}>
-                    <label>Name:
-                        <input name="name" type="text" required />
-                    </label>
-                    <label>Max Beneficiaries:
-                        <input name="max_ben" type="text" />
-                    </label>
-                    <label>Set Verification Code:
-                        <input name="code" type="text" required />
-                    </label>
-                    <label>Deadline:
-                        <input name="deadline" type="date" />
-                    </label>
-                    <label>Assign Admin:
-                        <select name="assignedAdmin">
-                            {!profiles ? (
-                                <option value="" disabled >No admin available</option>
-                            ) : (
-                                profiles.map((profile) => (
-                                    <option key={profile.id} value={profile.id}>{profile.name}</option>
-                                ))
-                            )}
-                        </select>
-                    </label>
-                    <button type="submit">Create</button>
-                </form>
-            </div>
+            <BatchForm />
         </div>
     )
 }
