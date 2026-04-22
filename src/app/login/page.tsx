9e212656a817c46eb06644e2ef0a01c815e9cb73
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-22 21:58:15 +0800

    Make LoginPage component asynchronous and add user authentication check

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,17 @@
 import style from "./page.module.css";
 import LoginForm from "./_components/LoginForm";
 import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
 
+    const supabase = await createClient()
 
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (user) {
+        redirect("/dashboard");
+    }
 
     return (
         <LoginForm />
