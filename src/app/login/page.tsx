9419fe6617f837e91398e63c99ca09c4712afc3a
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-22 21:54:41 +0800

    Add createClient import to LoginPage component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,11 @@
 import style from "./page.module.css";
 import LoginForm from "./_components/LoginForm";
+import { createClient } from "@/utils/supabase/server";
 
 export default function LoginPage() {
+
+
+
     return (
         <LoginForm />
     )
