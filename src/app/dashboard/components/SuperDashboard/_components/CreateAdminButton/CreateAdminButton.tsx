3b1978f0c1a5dfbee78cfe0a0fdc527c455ea1dd
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-24 22:26:21 +0800

    Implement modal for CreateAdminButton to enhance admin creation workflow

--- src/app/dashboard/components/SuperDashboard/_components/CreateAdminButton/CreateAdminButton.tsx
+++ src/app/dashboard/components/SuperDashboard/_components/CreateAdminButton/CreateAdminButton.tsx
@@ -1,7 +1,27 @@
+"use client";
 import style from "./CreateAdminButton.module.css";
+import { useState } from "react";
 
 export default function CreateAdminButton() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <button>Create new Admin</button>
+        <div className={style.mainDiv}>
+            <button onClick={() => setIsOpen(true)}>Create new Admin</button>
+
+            isOpen && (
+            <div className={style.modalOverlay}>
+                <div className={style.modalContent}>
+
+                    <div className={style.modalActions}>
+                        <button onClick={() => setIsOpen(false)}>Cancel</button>
+                        <button >Create</button>
+                    </div>
+                </div>
+            </div>
+            )
+        </div>
+
     )
 }
