0923e57b76926ebda7c56207047d87933fac8a54
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-26 22:27:25 +0800

    Add fetching of all admin profiles in SessionCard component

--- src/app/dashboard/components/SuperDashboard/_components/SessionCard/SessionCard.tsx
+++ src/app/dashboard/components/SuperDashboard/_components/SessionCard/SessionCard.tsx
@@ -38,6 +38,10 @@ export default async function SessionCard({ session }: SessionCardProps) {
         .eq("batch_id", session.id)
         .eq("status", "APPROVED");
 
+    const { data: allProfiles, error: profilesError } = await supabase
+        .from("profiles")
+        .select("id, name")
+        .eq("role", "ADMIN")
 
     return (
         <div className={style.mainDiv}>
