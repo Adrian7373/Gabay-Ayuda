94e0e2866d1fda4cd9dd1b0ab0bab0a388d864f6
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-04 22:32:49 +0800

    Refactor PersonalInfoCard component to accept props and define PersonalInfo interface; update RecordDetailsPage to include PersonalInfoCard and remove redundant profile details

--- src/app/records/[id]/_components/PersonalInfoCard/PersonalInfoCard.tsx
+++ src/app/records/[id]/_components/PersonalInfoCard/PersonalInfoCard.tsx
@@ -1,6 +1,26 @@
 
 
-export default function PersonalInfoCard() {
+interface PersonalInfo {
+    name: string,
+    religion: string,
+    bday: string,
+    contact: string,
+    email: string,
+    address: string,
+    age: string,
+    sex: string,
+    citizenship: string,
+    maritalStatus: string
+}
+
+interface PersonalInfoProps {
+    personalData: PersonalInfo
+}
+
+export default function PersonalInfoCard({ personalData }: PersonalInfoProps) {
+
+    const { name, religion, bday, contact, email, address, age, sex, citizenship, maritalStatus } = personalData;
+
     return (
         <>
             <p>Personal Info</p>
