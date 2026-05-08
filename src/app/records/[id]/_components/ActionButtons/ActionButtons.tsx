dafff9e8ee767a90959b7e615a4a8adaf9f25ce4
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-08 22:49:23 +0800

    Fix casing in ActionButtons component; standardize button text and header label

--- src/app/records/[id]/_components/ActionButtons/ActionButtons.tsx
+++ src/app/records/[id]/_components/ActionButtons/ActionButtons.tsx
@@ -6,10 +6,10 @@ import { SquarePen } from "lucide-react";
 export default function ActionButtons({ id }: { id: string }) {
     return (
         <>
-            <p><SquarePen height="3vh" width="1.5vw" />ACTIONS</p>
+            <p><SquarePen height="3vh" width="1.5vw" />Actions</p>
             <div className={style.buttonsDiv}>
-                <button onClick={() => updateRecordStatus(id, "APPROVED")}>Approve</button>
-                <button onClick={() => updateRecordStatus(id, "REJECTED")}>Reject</button>
+                <button onClick={() => updateRecordStatus(id, "APPROVED")}>APPROVE</button>
+                <button onClick={() => updateRecordStatus(id, "REJECTED")}>REJECT</button>
             </div>
         </>
 
