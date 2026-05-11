0658277cf0f330bb6a48eb7bca2ae62422228afc
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-11 20:46:00 +0800

    Refactor LoginForm component to enhance password visibility toggle and improve layout; update styles for better user experience

--- src/app/login/_components/LoginForm.tsx
+++ src/app/login/_components/LoginForm.tsx
@@ -3,12 +3,14 @@ import style from "./LoginForm.module.css";
 import { authenticateUser } from "@/app/actions";
 import { useTransition, useState } from "react";
 import { useRouter } from "next/navigation";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function LoginForm() {
 
     const router = useRouter();
     const [isAuthing, startTransition] = useTransition();
     const [message, setMessage] = useState<string>();
+    const [isShowing, setIsShowing] = useState(false);
 
     const handleSubmit = (formData: FormData) => {
         startTransition(async () => {
@@ -34,8 +36,11 @@ export default function LoginForm() {
                         <label>Username:
                             <input name="email" type="text" />
                         </label>
-                        <label>Password:
-                            <input name="password" type="text" />
+                        <label className={style.passwordLabel}>Password:
+                            <div>
+                                <input name="password" type={isShowing ? "text" : "password"} />
+                                <button type="button" className={style.showPasswordButton}>{isShowing ? <EyeOff width="1rem" height="1rem" /> : <Eye width="1rem" height="1rem" />}</button>
+                            </div>
                         </label>
                         <p>{message}</p>
                         <button className={style.loginButton} type="submit" >Login</button>
