c8c6d978337b087fd195113a435255ef67e7683a
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-14 22:20:49 +0800

    Enhance batch form layout: add assignForm class for consistent styling; update form structure for better organization

--- src/app/dashboard/configure/_components/batchForm.tsx
+++ src/app/dashboard/configure/_components/batchForm.tsx
@@ -76,52 +76,53 @@ export default function BatchForm({ profiles, initialData }: BatchFormProps) {
                     )}
                 </div>
             )}
-            <form action={async (formData) => {
+            <form className={style.assignForm} action={async (formData) => {
                 await createBatch(unassignedAdmins, formData);
             }}>
-                {isEditing && (
-                    <input type="hidden" name="batchId" value={initialData.batchId} />
-                )}
-                <div className={style.formDetails}>
-                    <label>Name:
-                        <input name="name" type="text" required defaultValue={initialData?.name} />
-                    </label>
-                    <label>Max Beneficiaries:
-                        <input name="max_ben" type="text" defaultValue={initialData?.max_approved} />
-                    </label>
-                    <label>Set Verification Code:
-                        <input name="code" type="text" required defaultValue={initialData?.verification_code} />
-                    </label>
-                    <label>Deadline:
-                        <input name="deadline" type="date" defaultValue={initialData?.deadline} />
-                    </label>
-                </div>
-                {isAddingAdmin ? (
-                    <>
-                        <label>Assign Admins:
-                            <div>
-                                {availableProfiles.length === 0 ? (
-                                    <p>No available admins (All assigned)</p>
-                                ) : (
-                                    availableProfiles.map((profile) => (
-                                        <label key={profile.id} className={style.checkboxItem}>
-                                            <input
-                                                type="checkbox"
-                                                name="assignedAdmins"
-                                                value={profile.id}
-                                            />
-                                            {profile.name}
-                                        </label>
-                                    ))
-                                )}
-                            </div>
+                <div className={style.formContent}>
+                    {isEditing && (
+                        <input type="hidden" name="batchId" value={initialData.batchId} />
+                    )}
+                    <div className={style.formDetails}>
+                        <label>Name:
+                            <input name="name" type="text" required defaultValue={initialData?.name} />
                         </label>
-                        <button type="button" onClick={() => setIsAddingAdmin(false)}>Cancel</button>
-                    </>
-                ) : (
-                    <button type="button" className={style.newAdminButton} onClick={() => setIsAddingAdmin(true)}>Add a new admin</button>
-                )}
-
+                        <label>Max Beneficiaries:
+                            <input name="max_ben" type="text" defaultValue={initialData?.max_approved} />
+                        </label>
+                        <label>Set Verification Code:
+                            <input name="code" type="text" required defaultValue={initialData?.verification_code} />
+                        </label>
+                        <label>Deadline:
+                            <input name="deadline" type="date" defaultValue={initialData?.deadline} />
+                        </label>
+                    </div>
+                    {isAddingAdmin ? (
+                        <div className={style.assignDiv}>
+                            <label>Assign Admins:
+                                <div>
+                                    {availableProfiles.length === 0 ? (
+                                        <p>No available admins (All assigned)</p>
+                                    ) : (
+                                        availableProfiles.map((profile) => (
+                                            <label key={profile.id} className={style.checkboxItem}>
+                                                <input
+                                                    type="checkbox"
+                                                    name="assignedAdmins"
+                                                    value={profile.id}
+                                                />
+                                                {profile.name}
+                                            </label>
+                                        ))
+                                    )}
+                                </div>
+                            </label>
+                            <button type="button" onClick={() => setIsAddingAdmin(false)}>Cancel</button>
+                        </div>
+                    ) : (
+                        <button type="button" className={style.newAdminButton} onClick={() => setIsAddingAdmin(true)}>Add a new admin</button>
+                    )}
+                </div>
                 <button type="submit">{initialData ? "Update" : "Create"}</button>
             </form>
         </>
